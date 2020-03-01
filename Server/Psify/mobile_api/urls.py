45229f7f7ea8f057c3625d98e0389f26753a82e5
notnamenot <izabela.pachel@wp.pl>
2020-03-01 09:06:58 +0100

    finish basic api

--- Server/Psify/mobile_api/urls.py
+++ Server/Psify/mobile_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include 
-from .views import MoodListCreateView, UserCreateView, UserSetDoctorView, UserLoginView
-#from rest_framework import routers
 from django.conf.urls import url
+from .views import MoodListCreateView, UserCreateView, UserSetDoctorView, UserLoginView, UserSetNotesView, UserSetDiagnosisView
+#from rest_framework import routers
 
 #router = routers.DefaultRouter()
 #router.register('moods', views.MoodListCreateView)	# unused on prod
@@ -11,6 +11,7 @@ urlpatterns = [
 	url(r'^moods/$', MoodListCreateView.as_view(), name='moods'),
 	url(r'^user-create/$', UserCreateView.as_view(), name='user-create'),
 	url(r'^user-login$', UserLoginView.as_view(), name='login-doctor'),
-	url(r'^user-update/$', UserSetDoctorView.as_view(), name='user-update')
-	
+	url(r'^user-update/$', UserSetDoctorView.as_view(), name='user-update'),
+	url(r'^user-notes/$', UserSetNotesView.as_view(), name='user-notes'),
+	url(r'^user-notes/$', UserSetDiagnosisView.as_view(), name='user-diagnosis')
 ]
